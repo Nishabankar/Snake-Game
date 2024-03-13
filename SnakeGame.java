6fe7979520fb7515e88ea48f5bc6d909d1a8b7a0
Nishabankar <105440065+Nishabankar@users.noreply.github.com>
2024-03-12 22:16:28 -0700

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,19 @@
+package bank;
+
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame{
+    SnakeGame(){
+        super("Snake Game");
+        add(new Board());
+        pack();
+
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+    
+        new SnakeGame();
+    }
+}
